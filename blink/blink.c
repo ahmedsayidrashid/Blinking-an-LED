0bb15d8bd8cf6b56e79c10c2c8c0d6f2884df944
Ahmed Rashid <98060670+ahmedsayidrashid@users.noreply.github.com>
2024-06-12 12:54:45 -0400

    Update blink.c
    Added more comments.
    Got rid of redundant code.

--- blink/blink.c
+++ blink/blink.c
@@ -15,22 +15,18 @@
 
 int main() {
 
-#ifndef PICO_DEFAULT_LED_PIN
-#warning blink example requires a board with a regular LED
-#else
     bool currentStatus = 0; // variable to keep track of the LED status
     stdio_init_all();
     gpio_init(BUTTON_GPIO); // initialize the button
     gpio_set_dir(BUTTON_GPIO, GPIO_IN);
     gpio_pull_down(BUTTON_GPIO); // use a pull down resistor to avoid flaots
     
-    gpio_init(LED);
+    gpio_init(LED); // initialize the GPIO for the LED
     gpio_set_dir(LED, GPIO_OUT);
     
     while(1){
-        currentStatus = gpio_get(BUTTON_GPIO) ? !currentStatus : currentStatus;
-        gpio_put(LED, currentStatus);  
-        gpio_put(CAPACITOR_CHARGE_GPIO, currentStatus);
+        currentStatus = gpio_get(BUTTON_GPIO) ? !currentStatus : currentStatus; // toggle the LED if button was pressed
+        gpio_put(LED, currentStatus);
         sleep_ms(500);
     }
 
